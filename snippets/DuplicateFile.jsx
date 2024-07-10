09cbc443258edd31d5ef9e61faf4aa229f89ce93
Josh Duncan <44387852+joshbduncan@users.noreply.github.com>
2024-07-10 13:21:53 -0400

    cleanup

--- snippets/DuplicateFile.jsx
+++
@@ -1,6 +0,0 @@
-// https://community.adobe.com/t5/illustrator-discussions/app-doscript-myapplescript-scriptlanguage-applescriptlanguage-is-not-working-in-illustrator/td-p/13170103
-
-var myFolder = new Folder(Folder.desktop + "/myMacFolder/");
-var myFile = new File(Folder.desktop + "/myMacFile.ai");
-var myFileCopy = new File(myFolder + "/myMacFile.ai");
-myFile.copy(myFileCopy);
